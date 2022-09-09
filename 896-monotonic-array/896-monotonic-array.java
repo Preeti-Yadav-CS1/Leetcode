15163ba6c0c3351034cc4f7a8a97a36f6d037cb1
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-09-09 23:59:59 +0530

    Time: 6 ms (17.31%), Space: 92.5 MB (78.69%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.java
+++ 896-monotonic-array/896-monotonic-array.java
@@ -3,7 +3,9 @@ class Solution {
         boolean inc= true;
         boolean dec= true;
         for(int i=0;i<nums.length-1;i++){
-            if(nums[i]>nums[i+1]) inc= false;
+            if(nums[i]>nums[i+1]){
+                inc= false;
+            } 
             if(nums[i]<nums[i+1]) dec= false;
         }
         return inc||dec;
