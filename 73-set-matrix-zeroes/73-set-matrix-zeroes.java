7a9f331def5113562e18091c87d6437a618deafa
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-09-12 22:07:15 +0530

    Time: 2 ms (48.75%), Space: 53.8 MB (71.94%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,55 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    //pivot up
+                    int idx=i-1;
+                    while(idx>=0){
+                        if(matrix[idx][j]!=0){
+                            matrix[idx][j]=-999;
+                        }
+                        idx--;
+                    }
+                    
+                    //pivot down
+                    idx=i+1;
+                    while(idx<row){
+                        if(matrix[idx][j]!=0){
+                            matrix[idx][j]=-999;
+                        }
+                        idx++;
+                    }
+                    
+                    //pivot left
+                    idx=j-1;
+                    while(idx>=0){
+                        if(matrix[i][idx]!=0){
+                            matrix[i][idx]=-999;
+                        }
+                        idx--;
+                    }
+                    
+                     //pivot dowm
+                    idx=j+1;
+                    while(idx<col){
+                        if(matrix[i][idx]!=0){
+                            matrix[i][idx]=-999;
+                        }
+                        idx++;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==-999){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
