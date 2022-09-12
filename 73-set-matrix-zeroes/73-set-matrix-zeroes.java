b20ca2ec2ca6c19cd34f65b82dff3a3fbb9b07b3
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-09-12 22:26:48 +0530

    Time: 2 ms (48.75%), Space: 54 MB (57.46%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -4,52 +4,22 @@ class Solution {
         int row=matrix.length;
         int col=matrix[0].length;
         
+        int rowTraverse[]= new int[row];
+        int colTraverse[]= new int[col];
+        Arrays.fill(rowTraverse,-1);
+        Arrays.fill(colTraverse,-1);
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(matrix[i][j]==0){
-                    //pivot up
-                    int idx=i-1;
-                    while(idx>=0){
-                        if(matrix[idx][j]!=0){
-                            matrix[idx][j]=-999;
-                        }
-                        idx--;
-                    }
-                    
-                    //pivot down
-                    idx=i+1;
-                    while(idx<row){
-                        if(matrix[idx][j]!=0){
-                            matrix[idx][j]=-999;
-                        }
-                        idx++;
-                    }
-                    
-                    //pivot left
-                    idx=j-1;
-                    while(idx>=0){
-                        if(matrix[i][idx]!=0){
-                            matrix[i][idx]=-999;
-                        }
-                        idx--;
-                    }
-                    
-                     //pivot dowm
-                    idx=j+1;
-                    while(idx<col){
-                        if(matrix[i][idx]!=0){
-                            matrix[i][idx]=-999;
-                        }
-                        idx++;
-                    }
+                    rowTraverse[i]= 0;
+                    colTraverse[j]=0;
                 }
             }
         }
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(matrix[i][j]==-999){
+                if(rowTraverse[i]==0||colTraverse[j]==0)
                     matrix[i][j]=0;
-                }
             }
         }
     }
