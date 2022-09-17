80e5e716f6e775166ecf48e130060d2327ca6981
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-09-18 02:03:02 +0530

    Time: 24 ms (26.17%), Space: 75 MB (73.26%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,22 +10,61 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ListNode currNode=head;
-        int count=0;
-        ArrayList<Integer> arr= new ArrayList<>();
-        while(currNode!=null){
-            arr.add(currNode.val);
-            count++;
-            currNode= currNode.next;
+        if(head==null || head.next==null)
+            return head;
+        //find mid
+        ListNode mid= getMid(head);
+        
+        //left right ms
+        ListNode rightHead= mid.next;
+        mid.next=null;
+        //mrege sort
+        ListNode newLeft= sortList(head);
+        ListNode newRight= sortList(rightHead);
+        
+        //merge
+        return merge(newLeft, newRight);
+    }
+    
+    //2 pointer approach
+    private ListNode getMid(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        currNode= head;
-        Collections.sort(arr);
-        int i=0;
-        while(currNode!=null){
-            currNode.val= arr.get(i);
-            currNode= currNode.next;
-            i++;
+        return slow;
+    }
+    
+    private ListNode merge(ListNode left, ListNode right){
+        ListNode dummy= new ListNode(-1);
+        ListNode currNode= dummy;
+        
+        while(left!=null && right!=null){
+            if(left.val<=right.val){
+                currNode.next=left;
+                left=left.next;
+                currNode=currNode.next;
+            }else{
+                currNode.next=right;
+                right=right.next;
+                currNode=currNode.next;
+            }
         }
-        return head;
+        
+        while(left!=null){
+            currNode.next=left;
+            left=left.next;
+            currNode=currNode.next;
+        }
+        while(right!=null){
+            currNode.next=right;
+            right=right.next;
+            currNode=currNode.next;
+        }
+        
+        return dummy.next;
     }
 }
