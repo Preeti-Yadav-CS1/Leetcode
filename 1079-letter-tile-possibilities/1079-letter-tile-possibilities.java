37be742f5a4452d34bfec4b5a61091500e1fa5ce
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-10-13 15:41:43 +0530

    Time: 18 ms (56.65%), Space: 42.3 MB (65.03%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numTilePossibilities(String s) {
+        int[] freq= new int[26];
+        for(int i=0;i<s.length();i++){
+            int idx= s.charAt(i)-'A';
+            freq[idx]++;
+        }
+        // for(int i=0;i<26;i++){
+        //     System.out.print(freq[i]+" ");
+        // }
+        int result=function(freq, ""); 
+        return result;
+    }
+    public static int function(int[] freq, String ans){
+        // System.out.println(ans);
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]>=1){
+                char ch=(char)(i+'A');
+                freq[i]--;
+                count+= function(freq, ans+ch)+1;
+                freq[i]++;
+            }
+        }
+        return count;
+    }
+}
