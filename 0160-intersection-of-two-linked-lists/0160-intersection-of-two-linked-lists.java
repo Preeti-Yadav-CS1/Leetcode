c46a790ce8390f73ab2ae6e1285ea598b32bf30e
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-10-16 03:57:34 +0530

    Time: 549 ms (6.01%), Space: 55.4 MB (47.40%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,21 +11,35 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA==null || headB==null) return null;
-        ListNode a_pointer= headA;
-        ListNode b_pointer= headB;
-        while(a_pointer!= b_pointer){
-            if(a_pointer==null){
-                a_pointer=headB;
-            }else{
-                a_pointer= a_pointer.next;
+        //approach 1
+        // if(headA==null || headB==null) return null;
+        // ListNode a_pointer= headA;
+        // ListNode b_pointer= headB;
+        // while(a_pointer!= b_pointer){
+        //     if(a_pointer==null){
+        //         a_pointer=headB;
+        //     }else{
+        //         a_pointer= a_pointer.next;
+        //     }
+        //     if(b_pointer==null){
+        //         b_pointer=headA;
+        //     }else{
+        //         b_pointer= b_pointer.next;
+        //     }
+        // }
+        // return a_pointer;
+        
+        // approach 2
+        while(headB!=null){
+            ListNode currNode= headA;
+            while(currNode!=null){
+                if(currNode==headB){
+                    return headB;
+                }
+                currNode= currNode.next;
             }
-            if(b_pointer==null){
-                b_pointer=headA;
-            }else{
-                b_pointer= b_pointer.next;
-            }
+            headB= headB.next;
         }
-        return a_pointer;
+        return null;  
     }
 }
