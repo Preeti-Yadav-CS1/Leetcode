c9521e300fe483b67d1f1abeb06777590130679b
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-10-16 03:58:47 +0530

    Time: 1 ms (99.80%), Space: 44.9 MB (97.15%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -12,34 +12,34 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         //approach 1
-        // if(headA==null || headB==null) return null;
-        // ListNode a_pointer= headA;
-        // ListNode b_pointer= headB;
-        // while(a_pointer!= b_pointer){
-        //     if(a_pointer==null){
-        //         a_pointer=headB;
-        //     }else{
-        //         a_pointer= a_pointer.next;
-        //     }
-        //     if(b_pointer==null){
-        //         b_pointer=headA;
-        //     }else{
-        //         b_pointer= b_pointer.next;
-        //     }
-        // }
-        // return a_pointer;
+        if(headA==null || headB==null) return null;
+        ListNode a_pointer= headA;
+        ListNode b_pointer= headB;
+        while(a_pointer!= b_pointer){
+            if(a_pointer==null){
+                a_pointer=headB;
+            }else{
+                a_pointer= a_pointer.next;
+            }
+            if(b_pointer==null){
+                b_pointer=headA;
+            }else{
+                b_pointer= b_pointer.next;
+            }
+        }
+        return a_pointer;
         
         // approach 2
-        while(headB!=null){
-            ListNode currNode= headA;
-            while(currNode!=null){
-                if(currNode==headB){
-                    return headB;
-                }
-                currNode= currNode.next;
-            }
-            headB= headB.next;
-        }
-        return null;  
+        // while(headB!=null){
+        //     ListNode currNode= headA;
+        //     while(currNode!=null){
+        //         if(currNode==headB){
+        //             return headB;
+        //         }
+        //         currNode= currNode.next;
+        //     }
+        //     headB= headB.next;
+        // }
+        // return null;  
     }
 }
