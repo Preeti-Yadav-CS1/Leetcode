4dab530aa6daf9e5d15dcc6497f62ed122b36eb1
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-10-18 01:13:51 +0530

    Time: 1 ms (97.45%), Space: 54.3 MB (27.26%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,26 @@
+//time complexity O(n*m*(n+m))*O(n*m)
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+        
+        int rowTraverse[]= new int[row];
+        int colTraverse[]= new int[col];
+        Arrays.fill(rowTraverse,-1);
+        Arrays.fill(colTraverse,-1);
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    rowTraverse[i]= 0;
+                    colTraverse[j]=0;
+                }
+            }
+        }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(rowTraverse[i]==0||colTraverse[j]==0)
+                    matrix[i][j]=0;
+            }
+        }
+    }
+}
