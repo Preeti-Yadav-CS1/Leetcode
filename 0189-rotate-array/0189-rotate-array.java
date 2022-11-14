512cb9ecc60b5420eed7560d72d0b7b048757828
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-11-14 21:08:13 +0530

    Time: 1 ms (91.61%), Space: 64.9 MB (10.01%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,12 +2,37 @@ class Solution {
     
     public void rotate(int[] nums, int k) {
         // approach 1: reversal O(1)- space complexity O(n)- time complexity
+        
         k= k%nums.length;
         //length of frist part
         int len= nums.length-k;
         reverse(nums,0,len-1);
         reverse(nums, len, nums.length-1);
         reverse(nums, 0, nums.length-1);
+        
+        //approach 2:Intermediate Array Approach
+        // int arr[]= new int[nums.length];
+        // for(int i=0;i<k;i++){
+        //     arr[i]= nums[nums.length-k+i];
+        // }
+        // int j=0;
+        // for(int i=k;i<nums.length;i++){
+        //     arr[i]= nums[j];
+        //     j++;
+        // }
+        // System.arraycopy(arr,0,nums,0, nums.length);
+        
+        //
+        //approach 3: Bubble Rotate
+//         for(int i=0;i<k;i++){
+//             for(int j=nums.length-1;j>0;j--){
+//                 int temp= nums[j];
+//                 nums[j]= nums[j-1];
+//                 nums[j-1]= temp;
+                
+//             }
+//         }
+        
     }
     public void reverse(int[] nums, int start, int end){
         while(start<end){
