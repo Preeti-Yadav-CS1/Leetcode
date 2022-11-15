c069955bd44f97d47e75de62b814af88059bc6eb
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-11-15 11:46:11 +0530

    Time: 209 ms (10.72%), Space: 117.2 MB (12.85%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        
+        //base case
+        if(p.length()==0){
+            return s.length()==0;
+        }
+        
+        //special case
+        if(p.length()==1){
+            
+            //
+            if(s.length()==0)
+                return false;
+            else if(p.charAt(0)!= s.charAt(0) && p.charAt(0)!='.')
+                return false;
+            else
+                return isMatch(s.substring(1),p.substring(1));
+        }
+        
+        //case 1: second character of p is not '*'
+        if(p.charAt(1)!='*'){
+            if(s.length()<1){
+                return false;
+            }
+            if(p.charAt(0)!= s.charAt(0) && p.charAt(0)!='.'){
+                return false;
+            }else{
+                return isMatch(s.substring(1),p.substring(1));
+            }
+        }
+        
+        //case 2: when the second part of p is '*', complex case:
+        else{
+            //case 2.1: a char & '*' can be stand for 0 element
+            if(isMatch(s, p.substring(2))){
+                return true;
+            }
+            
+            //case 2.2: a char & '*' can stand for 1 or more preceding element,
+            // so try every substring
+            int i=0;
+            while(i<s.length() && (s.charAt(i)==p.charAt(0) || p.charAt(0)=='.')){
+                if(isMatch(s.substring(i+1), p.substring(2))){
+                    return true;
+                }
+                i++;
+            }
+            return false;
+        }
+    }
+}
