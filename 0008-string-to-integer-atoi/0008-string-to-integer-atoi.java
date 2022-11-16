e9d37060e1e864011c516654c0da5ffe09c459c8
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-11-16 23:17:28 +0530

    Time: 18 ms (9.33%), Space: 44.8 MB (5.18%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int myAtoi(String s) {
+        
+        //root condition
+        if(s.length()<1 || s==null){
+            return 0;
+        }
+        
+        final int max= Integer.MAX_VALUE;
+        final int min= Integer.MIN_VALUE;
+        
+        //trip all spaces
+        s= s.replaceAll("^\\s+","");
+        
+        int count=0;
+        
+        //check if number is positive or negative
+        
+        boolean isNegative= s.startsWith("-");
+        boolean isPositive= s.startsWith("+");
+        
+        if(isNegative ||isPositive)
+            count++;
+        
+        //storage of converted number
+        
+        double number=0;
+        
+        while(count<s.length() && s.charAt(count)>='0' && s.charAt(count)<='9'){
+            number= number*10+(s.charAt(count)-'0');
+            count++;
+        }
+        number= isNegative? -number:number;
+        return (int) number;
+    }
+}
