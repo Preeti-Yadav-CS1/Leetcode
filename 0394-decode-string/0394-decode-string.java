ef263c710d12578c8a12add28e7a527824103e96
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-11-24 10:13:05 +0530

    Time: 10 ms (14.39%), Space: 42.7 MB (10.61%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,34 +1,35 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> counts= new Stack();
+        Stack<Integer> stack= new Stack();
         Stack<String> result= new Stack();
-        String res= "";
+        String res="";
         int i=0;
         while(i<s.length()){
             if(Character.isDigit(s.charAt(i))){
                 int count=0;
                 while(Character.isDigit(s.charAt(i))){
-                    count=10*count+(s.charAt(i)-'0');
+                    count=count*10+ (s.charAt(i)-'0');
                     i++;
                 }
-                counts.push(count);
+                stack.push(count);
             }else if(s.charAt(i)=='['){
                 result.push(res);
                 res="";
                 i++;
             }else if(s.charAt(i)==']'){
-                StringBuilder str= new StringBuilder(result.pop());
-                int count= counts.pop();
+                StringBuilder sb= new StringBuilder(result.pop());
+                int count=stack.pop();
                 for(int j=0;j<count;j++){
-                    str.append(res);
+                    sb.append(res);
                 }
-                res=str.toString();
+                res= sb.toString();
                 i++;
             }else{
                 res+=s.charAt(i);
                 i++;
             }
         }
+        
         return res;
     }
 }
