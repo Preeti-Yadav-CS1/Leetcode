daf514851b28671fded7e2c358b5435a4d0c2664
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2022-11-24 10:46:14 +0530

    Time: 32 ms (5.23%), Space: 46.6 MB (5.10%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,30 +1,36 @@
 class Solution {
     public String simplifyPath(String path) {
         Stack<String> stack= new Stack();
-        String result="";
+        String result= "";
         result+="/";
         
         int len= path.length();
+        
+        //compress
         for(int i=0;i<len;i++){
             String res="";
             while(i<len && path.charAt(i)=='/'){
                 i++;
-            }
-            while(i<len && path.charAt(i)!='/'){
+            }while(i<len && path.charAt(i)!='/' ){
                 res+=path.charAt(i);
                 i++;
             }
+            
             if(res.equals("..")==true){
                 if(!stack.empty()){
                     stack.pop();
                 }
-            }else if(res.equals(".")==true){
-                continue;
             }
-            else if(res.length()!=0){
+            else if(res.equals(".")==true){
+                continue;
+            }else if(res.length()!=0){
                 stack.push(res);
             }
         }
+        
+        
+        //result
+        
         Stack<String> st= new Stack();
         while(!stack.empty()){
             st.push(stack.pop());
@@ -32,7 +38,7 @@ class Solution {
         
         while(!st.empty()){
             if(st.size()!=1){
-                result+= (st.pop()+"/");
+                result+=(st.pop()+'/');
             }else{
                 result+=st.pop();
             }
