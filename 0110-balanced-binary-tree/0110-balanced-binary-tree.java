a235c53163e01e751e77b326c7727d44a73de46d
Anonymous <73437019+Preeti-Yadav-CS1@users.noreply.github.com>
2023-01-19 14:51:45 +0530

    Time: 151 ms (5.12%), Space: 42.5 MB (43.30%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,23 +14,38 @@
  * }
  */
 class Solution {
+    class DiaPair{
+        int ht=0;
+        int d=0;
+    }
     public boolean isBalanced(TreeNode root) {
         if(root==null) return true;
         
         boolean Left= isBalanced(root.left);
         boolean Right= isBalanced(root.right);
         
-        int absolute= Math.abs(height(root.left)-height(root.right));
+        int absolute= Math.abs(diameter(root.left).ht-diameter(root.right).ht);
         
         return Left&&Right&&(absolute<=1);
         
     }
-    public int height(TreeNode root){
-        if(root==null) return 0;
+//     public int height(TreeNode root){
+//         if(root==null) return 0;
         
-        int left= height(root.left);
-        int right= height(root.right);
+//         int left= height(root.left);
+//         int right= height(root.right);
         
-        return Math.max(left,right)+1;
+//         return Math.max(left,right)+1;
+//     }
+     public DiaPair diameter(TreeNode root){
+        if(root==null) return new DiaPair();
+        DiaPair ldp= diameter(root.left);
+        DiaPair rdp= diameter(root.right);
+        DiaPair self= new DiaPair();
+        int sd= ldp.ht+ rdp.ht;
+        
+        self.d= Math.max(Math.max(ldp.d, rdp.d), sd);
+        self.ht= Math.max(ldp.ht, rdp.ht)+1;
+        return self;
     }
 }
